b16e11469e75438e9ca9e414457cb54496923061
oscarciceri <oscarcicericoral@gmail.com>
2021-12-20 18:04:31 -0300

    import excel in a df with R

---
+++ importExcel.R
@@ -0,0 +1,24 @@
+library(readxl)
+
+
+#Search the file
+file.choose()
+
+pwd <- "C:\\Users\\oscar\\Documents\\Git\\Rprojects\\data.xlsx"
+
+
+#See the leaf excel
+excel_sheets(pwd)
+
+#Read Excel
+# First Case
+ideal_case <- read_excel(pwd)
+class(ideal_case)
+
+# Second Case
+second_case <- read_excel(pwd, sheet = "Hoja2")
+
+# Complex case
+complex_case <- read_excel(pwd, 
+                           sheet = "Hoja3",
+                           range = 'C7:F17')
