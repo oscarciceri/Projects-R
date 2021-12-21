789ac0a0b576210a2fcbfc81bcb92570e56b4064
oscarciceri <oscarcicericoral@gmail.com>
2021-12-21 01:17:08 -0300

    plots

--- ggplot2.R
+++ ggplot2.R
@@ -1,1 +1,51 @@
+# Data
+year <- seq(2010, 2018, by=1)
+disney <- c(11, 13, 11, 8, 12, 11, 12, 8, 10)
+
+
+plot(year, disney)
+
+plot(x = year,
+     y = disney,
+     main = 'Title movies',
+     xlab =  'Disney',
+     ylab =  'Years',
+     col =   'cornflowerblue',
+     pch = 16,
+     panel.first = grid())
+
+barplot(disney,
+        main = "Score Disney",
+        xlab = "Score",
+        ylab = "Years",
+        names.arg = year,
+        col = "darkred",
+        horiz = TRUE)
+
+hist(disney)
+
+
+total <- sum(disney)
+percentage <- disney/total
+percentage
+percentage <- round(percentage, 3)
+percentage
+
+pie(percentage,
+    labels = percentage,
+    col = rainbow(9),
+    cex = 0.9)
+
+
 library(ggplot2)
+
+# create the data frame
+movies <- data.frame(year, disney)
+
+ggplot(data = movies,
+       mapping = aes(x = year,
+                     y = disney)) +
+  geom_point() +
+  labs(title = 'disney')
+
+
