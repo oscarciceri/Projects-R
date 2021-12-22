22f0bf166e0bcd859c9a74a1884922b4470806c0
oscarciceri <oscarcicericoral@gmail.com>
2021-12-21 22:52:44 -0300

    EDA

---
+++ Code/EDA.R
@@ -0,0 +1,37 @@
+library(gapminder)
+data(gapminder)
+
+library(tidyverse)
+
+#EDA exploratory data analysis
+resumen <- as.data.frame.matrix(summary(gapminder))
+
+
+hist(gapminder$lifeExp)
+
+head(gapminder$lifeExp)
+
+ggplot(data=gapminder, aes(lifeExp)) + 
+  geom_histogram()
+
+
+ggplot(data = gapminder,
+       mapping = aes(x = continent,
+                     y = lifeExp,
+                     fill = continent)) +
+  geom_boxplot() 
+  #+ geom_jitter()
+ 
+  
+ggplot(data = gapminder,
+       mapping = aes(x = continent,
+                     y = lifeExp,
+                     fill = continent)) +
+  geom_violin() 
+
+data <- data.frame(gapminder$lifeExp, 
+                   gapminder$continent,
+                   gapminder$gdpPercap)
+pairs(data)  
+
+
