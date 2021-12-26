2f877f07ce1b0cd4bee34e903b707ac3b2bcec71
oscarciceri <oscarcicericoral@gmail.com>
2021-12-26 10:06:49 -0300

    dataframe

--- Code/05_dataframe.R
+++ Code/05_dataframe.R
@@ -1,8 +1,8 @@
 #Data frame
 
-name <- c("name1", "name2", "name3")
-score <- c(3.5, 9.2, 8)
-winner <- c(TRUE, FALSE, FALSE)
+name <- c("name1", "name2", "name3", "name4")
+score <- c(3.5, 9.2, 8, 5)
+winner <- c(TRUE, FALSE, FALSE, FALSE)
 
 #Vector require to have the same size
 films_df <- data.frame(name, score, winner)
@@ -20,7 +20,7 @@ films_df[1,"Score"]
 
 
 #Select more than one element
-films_df[c(1,2), c(2,3)]
+films_df[c(1,2,4), c(2,3)]
 films_df[c(1,2,3), c('Score', 'Winner')]
 
 #select row our column
